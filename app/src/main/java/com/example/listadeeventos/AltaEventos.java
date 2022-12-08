cf58130ac3db2726a737296214464506a38afba0
vnavacar <vnavacar@myuax.com>
2022-12-08 15:56:29 +0100

    empezado

---
+++ app/src/main/java/com/example/listadeeventos/AltaEventos.java
@@ -0,0 +1,174 @@
+package com.example.listadeeventos;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+
+import java.text.ParseException;
+
+public class AltaEventos extends Activity implements View.OnClickListener {
+
+    private static final int FOTO_EVENTO = 1;
+    private String accion;
+    private long idEvento;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alta_eventos);
+
+        Button btAlta = findViewById(R.id.btAlta);
+        btAlta.setOnClickListener(this);
+        Button btCancelar = findViewById(R.id.btCancelar);
+        btCancelar.setOnClickListener(this);
+        ImageButton ibImagen = findViewById(R.id.ibImagen);
+        ibImagen.setOnClickListener(this);
+
+        accion = getIntent().getStringExtra("accion");
+        if (accion.equals("modificar")) {
+            Evento evento = (Evento) getIntent().getSerializableExtra("evento");
+            Bitmap imagenEvento = Util.getBitmap(getIntent().getByteArrayExtra("imagen"));
+            rellenarDatos(evento, imagenEvento);
+            btAlta.setText(R.string.guardar);
+        }
+    }
+
+    private void rellenarDatos(Evento evento, Bitmap imagenEvento) {
+        EditText etNombre = findViewById(R.id.etNombre);
+        EditText etDescripcion = findViewById(R.id.etDescripcion);
+        EditText etDireccion = findViewById(R.id.etDireccion);
+        EditText etFecha = findViewById(R.id.etFecha);
+        EditText etPrecio = findViewById(R.id.etPrecio);
+        EditText etAforo = findViewById(R.id.etAforo);
+        ImageButton ibImagen = findViewById(R.id.ibImagen);
+
+        etNombre.setText(evento.getNombre());
+        etDescripcion.setText(evento.getDescripcion());
+        etDireccion.setText(evento.getDireccion());
+        etFecha.setText(Util.formatearFecha(evento.getFecha()));
+        etPrecio.setText(String.valueOf(evento.getPrecio()));
+        etAforo.setText(String.valueOf(evento.getAforo()));
+        ibImagen.setImageBitmap(imagenEvento);
+
+        idEvento = evento.getId();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btAlta:
+                EditText etNombre = findViewById(R.id.etNombre);
+                EditText etDescripcion = findViewById(R.id.etDescripcion);
+                EditText etDireccion = findViewById(R.id.etDireccion);
+                EditText etFecha = findViewById(R.id.etFecha);
+                EditText etPrecio = findViewById(R.id.etPrecio);
+                EditText etAforo = findViewById(R.id.etAforo);
+                ImageButton ibImagen = findViewById(R.id.ibImagen);
+
+                try {
+                    if (etPrecio.getText().toString().equals("")) {
+                        Toast.makeText(this, R.string.mensaje_precio,
+                                Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    if (etAforo.getText().toString().equals(""))
+                        etAforo.setText("0");
+
+                    Evento evento = new Evento();
+                    evento.setNombre(etNombre.getText().toString());
+                    evento.setDescripcion(etDescripcion.getText().toString());
+                    evento.setDireccion(etDireccion.getText().toString());
+                    evento.setFecha(Util.parsearFecha(etFecha.getText().toString()));
+                    evento.setPrecio(Float.parseFloat(etPrecio.getText().toString()));
+                    evento.setAforo(Integer.parseInt(etAforo.getText().toString()));
+                    evento.setImagen(((BitmapDrawable) ibImagen.getDrawable()).getBitmap());
+
+                    Database db = new Database(this);
+                    switch (accion) {
+                        case "nuevo":
+                            db.nuevoEvento(evento);
+                            break;
+                        case "modificar":
+                            evento.setId(idEvento);
+                            db.modificarEvento(evento);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    Toast.makeText(this, "El evento " + evento.getNombre() +
+                            " ha sido guardado", Toast.LENGTH_LONG).show();
+
+                    etNombre.setText("");
+                    etNombre.requestFocus();
+                    etDescripcion.setText("");
+                    etDireccion.setText("");
+                    etPrecio.setText("");
+                    etAforo.setText("");
+                    etFecha.setText("");
+                } catch (ParseException pe) {
+                    Toast.makeText(this, "Formato de fecha no válido", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.btCancelar:
+                onBackPressed();
+                break;
+            case R.id.ibImagen:
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, FOTO_EVENTO);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if ((resultCode == RESULT_OK) && (data != null)) {
+
+            switch (requestCode) {
+                case FOTO_EVENTO:
+                    // Obtiene el Uri de la imagen seleccionada por el usuario
+                    Uri imagenSeleccionada = data.getData();
+                    String[] ruta = {MediaStore.Images.Media.DATA };
+
+                    // Realiza una consulta a la galería de imágenes solicitando la imagen seleccionada
+                    Cursor cursor = getContentResolver().query(imagenSeleccionada, ruta, null, null, null);
+                    cursor.moveToFirst();
+
+                    // Obtiene la ruta a la imagen
+                    int indice = cursor.getColumnIndex(ruta[0]);
+                    String picturePath = cursor.getString(indice);
+                    cursor.close();
+
+                    // Carga la imagen en una vista ImageView que se encuentra en
+                    // en layout de la Activity actual
+                    ImageButton ibImagen = findViewById(R.id.ibImagen);
+                    ibImagen.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/listadeeventos/MainActivity.java
+++ app/src/main/java/com/example/listadeeventos/MainActivity.java
@@ -1,71 +1,106 @@
 package com.example.listadeeventos;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import com.example.listadeeventos.databinding.ActivityMainBinding;
-
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private List<Evento> eventos;
+    private EventoAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        Database db = new Database(this);
+        eventos = db.getEventos();
 
-        setSupportActionBar(binding.toolbar);
+        ListView lvEventos = findViewById(R.id.lvEventos);
+        adapter = new EventoAdapter(this,
+                R.layout.item_evento, eventos);
+        lvEventos.setAdapter(adapter);
+        registerForContextMenu(lvEventos);
+    }
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    @Override
+    protected void  onResume() {
+        super.onResume();
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        refrescarListaEventos();
+    }
+
+    private void refrescarListaEventos() {
+
+        eventos.clear();
+        Database db = new Database(this);
+        eventos.addAll(db.getEventos());
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_contextual, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo menuInfo =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int posicion = menuInfo.position;
+
+        switch (item.getItemId()) {
+            case R.id.menu_modificar:
+                Intent intent = new Intent(this, AltaEventos.class);
+                Evento evento = eventos.get(posicion);
+                intent.putExtra("accion", "modificar");
+                intent.putExtra("evento", evento);
+                intent.putExtra("imagen", Util.getBytes(evento.getImagen()));
+                startActivity(intent);
+                return true;
+            case R.id.menu_eliminar:
+                Database db = new Database(this);
+                db.eliminarEvento(eventos.get(posicion));
+                refrescarListaEventos();
+                return true;
+            default:
+                return false;
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.item_alta_eventos:
+                Intent intent = new Intent(this, AltaEventos.class);
+                intent.putExtra("accion", "nuevo");
+                startActivity(intent);
+                return true;
+            default:
+                return false;
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
